647cfc7b1c8473c4dbab9cd344a76fe818c30d64
Leandro Marques <leandrojerse@gmail.com>
2025-01-25 12:08:07 -0300

    First commit before to start

---
+++ src/main/java/org/example/aulasnelio/AulasNelioApplication.java
@@ -0,0 +1,13 @@
+package org.example.aulasnelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasNelioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AulasNelioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/aulasnelio/AulasNelioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aulasnelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulasNelioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
